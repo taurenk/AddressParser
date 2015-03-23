9265391c1d377e9cdd99b6c5aa494c571dd6217d
taurenk <tauren.kristich@gmail.com>
2015-03-22 20:22:06 -0400

    TK 03/22/2015 1. Working on RegexLibrary for loading/creating address standards regex/maps.

--- src/main/java/com/taurenk/addressparser/regexlibrary/Standards.java
+++
@@ -1,9 +0,0 @@
-package com.taurenk.addressparser.regexlibrary;
-
-/**
- * Created by tauren on 3/21/15.
- */
-public class Standards {
-
-
-}
---
+++ src/test/java/com/taurenk/addressparser/testRegex.java
@@ -0,0 +1,16 @@
+package com.taurenk.addressparser;
+
+
+import com.taurenk.addressparser.regexlibrary.RegexLibrary;
+
+/**
+ * Created by tauren on 3/22/15.
+ */
+public class testRegex {
+
+    public static void main(String[] args) {
+       RegexLibrary regexLib = new RegexLibrary();
+
+
+    }
+}
